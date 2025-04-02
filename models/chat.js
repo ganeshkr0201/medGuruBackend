c03e6da0b80bfef25375a2f5152f4d24671d699e
Ganesh Kumar <142982950+ganeshkr0201@users.noreply.github.com>
2025-04-02 20:03:28 +0530

    Update chat.js

--- models/chat.js
+++ models/chat.js
@@ -1,1 +1,27 @@
+import mongoose from "mongoose";
 
+const chatSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    sessionId: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    messages: [
+        {
+            text: { type: String, required: true },
+            user: { type: String, enum: ["me", "ai"], required: true },
+            timestamp: { type: Date, default: Date.now }
+        }
+    ]
+}, { timestamps: true });
+
+const Chat = mongoose.model("Chat", chatSchema);
+export { Chat };

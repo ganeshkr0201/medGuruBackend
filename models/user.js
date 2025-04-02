6aa092d21bedb5acfad73b03916b01044b3e5ba3
Ganesh Kumar <142982950+ganeshkr0201@users.noreply.github.com>
2025-04-02 20:02:45 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,46 +1,27 @@
 import mongoose from "mongoose";
 
-const chatSchema = new mongoose.Schema({
-    user: {
-        type: String,
-        required: true
-    },
-    ai: {
-        type: String,
-        required: true
-    },
-    timestamp: {
-        type: Date,
-        default: Date.now
-    }
-})
-
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
+    name: { 
+        type: String, 
+        required: true, 
+        trim: true 
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true
+        type: String, 
+        required: true, 
+        unique: true, 
+        lowercase: true 
     },
-    password: {
-        type: String,
-        required: true
+    password: { 
+        type: String, 
+        required: true 
     },
-    chatHistory: [chatSchema],
     createdAt: {
-        type: Date,
-        default: Date.now
-    }
-})
+        type: Date, 
+        default: Date.now 
+    },
+});
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
-export {
-    User
-}
+export { User };

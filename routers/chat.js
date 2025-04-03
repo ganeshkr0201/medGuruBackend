55549304b9982546a4845586a4a3beacb0931234
Ganesh Kumar <142982950+ganeshkr0201@users.noreply.github.com>
2025-04-03 11:42:40 +0530

    Update chat.js

--- routers/chat.js
+++ routers/chat.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { handleReqResFromAi, getUserChatHistory, deleteChatHistory } from '../controllers/chat.js';
+import { handleReqResFromAi, getUserChatHistory, deleteChatHistory, getUserChatSessionHistory } from '../controllers/chat.js';
 
 const chatRouter = express.Router();
 
-chatRouter.get("/history/:user_id", getUserChatHistory);
-chatRouter.delete("/history/:user_id/:sessionId", deleteChatHistory);
 chatRouter.post('/:user_id/:sessionId', handleReqResFromAi);
+chatRouter.get("/history/:user_id", getUserChatHistory);
+chatRouter.get("/history/:user_id/:sessionId", getUserChatSessionHistory);
+chatRouter.delete("/history/:user_id/:sessionId", deleteChatHistory);
 
 
 export default chatRouter;

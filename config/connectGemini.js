94017189ae8a85bb13f92f8985d5cb91bff62080
Ganesh Kumar <142982950+ganeshkr0201@users.noreply.github.com>
2025-04-04 23:38:51 +0530

    Update connectGemini.js

--- config/connectGemini.js
+++ config/connectGemini.js
@@ -6,10 +6,10 @@ const model = new ChatGoogleGenerativeAI({
     apiKey: process.env.GEMINI_API_KEY
 });
 
-const sendReqToGemini = async (prompt) => {
+const sendReqToGemini = async (prompt, history) => {
     const messages = [
-        new SystemMessage("You are a Mindfulness Meditation Assistant you must Guides users through meditation and mindfulness exercises and if user ask anything beyond meditaion and mindrelaxation you must reject their questions and tell him you can't answer that and you can also suggest external video or image links related to the mediation"),
-        new HumanMessage(prompt)
+        new SystemMessage("You are a Mindfulness Meditation Assistant you must Guides users through meditation and mindfulness exercises and if user ask anything beyond strees reliefe, meditaion and mindrelaxation or related to these terms, you must reject their questions and tell him you can't answer that but if user asks about their previous chat history or related to chat history you must respond to that and you can also suggest external video or image links related to the mediation i'll provide you two human message the first one will be the prompt most imporatant and the second one will be the history of our previous conversations if user asks queries about their chat history you must reply from the second human message i.e; history."),
+        new HumanMessage(`This is the actual prompt ${prompt} and this is the chat history ${history}`),
     ];
     
     return await model.invoke(messages);

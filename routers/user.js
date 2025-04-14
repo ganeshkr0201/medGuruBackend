e5d6a2deb24b4dd647b8fc633e14cc4897492222
Ganesh Kumar <142982950+ganeshkr0201@users.noreply.github.com>
2025-04-14 22:10:34 +0530

    Update user.js

--- routers/user.js
+++ routers/user.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { handleLogout, handleSignIn, handleSignUp } from '../controllers/user.js';
+import { handleDelete, handleLogout, handleSignIn, handleSignUp } from '../controllers/user.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/signup', handleSignUp);
 userRouter.post('/signin', handleSignIn);
 userRouter.get('/logout', handleLogout);
+userRouter.post('/delete/:userId', handleDelete);
 
 export default userRouter;
